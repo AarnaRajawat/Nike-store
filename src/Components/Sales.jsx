17344fde3d582a66e11c76f7f7fd5e22aeb0ebe4
APARNA RAJAWAT <156735906+AarnaRajawat@users.noreply.github.com>
2025-01-24 15:46:12 +0530

    Update Sales.jsx

--- src/Components/Sales.jsx
+++ src/Components/Sales.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Title from './utils/Title'
-import Item from './utils/Item'
+import Title from './utils/Title.jsx'
+import Item from './utils/Item.jsx'
 
 const Sales = ({ifExists ,endpoint:{title,items}}) => {
 
